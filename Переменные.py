3040f16fcedd3b46c81dd5b7ea62a768677d5223
VadimCine <aka-sem@yandex.ru>
2024-08-21 23:01:18 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,5 @@
+count_homework = "12"
+count_hours = "1.5"
+course_title = "Python"
+task_time = 1.5 / 12
+print("Курс:", course_title + ",", "всего задач:" + count_homework + ",", "затрачено часов:" +  count_hours + ",",  "среднее время выполнения",  task_time, "часа.")
