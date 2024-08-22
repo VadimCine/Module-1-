9be578692d0ab9bbd47e716639633bababe7c0f7
VadimCine <aka-sem@yandex.ru>
2024-08-22 09:03:07 +0300

    Переменные.py
    Исправления

--- Переменные.py
+++ Переменные.py
@@ -1,5 +1,7 @@
-count_homework = "12"
-count_hours = "1.5"
+count_homework = 12
+count_hours = 1.5
 course_title = "Python"
-task_time = 1.5 / 12
-print("Курс:", course_title + ",", "всего задач:" + count_homework + ",", "затрачено часов:" +  count_hours + ",",  "среднее время выполнения",  task_time, "часа.")
+task_time = count_hours / count_homework
+
+
+print("Курс:", course_title + ",", "всего задач:", count_homework, ",", "затрачено часов:",  count_hours, ",",  "среднее время выполнения",  task_time, "часа.")
